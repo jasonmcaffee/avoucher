ca7ead1650ca54f29854eab221e8256820214168
Jason Mcaffee <jason.mcaffee@insidesales.com>
2018-01-14 11:43:00 -0700

    schema numeric types testing wip.

--- validation_plugins/validate_type_test.go
+++ validation_plugins/validate_type_test.go
@@ -10,8 +10,9 @@ import (
 	"reflect"
 )
 
+//GoNumericTypeInstances contains instances for every Numeric type defined in Go.
 type GoNumericTypeInstances struct{
-	//cache of all property values on this object
+	//cache of all property values on this object (built when NewGoNumericTypeInstances is called)
 	AllNumericTypeInstances []interface{}
 	//Numeric Types - https://golang.org/ref/spec#Numeric_types
 	//uint8       the set of all unsigned  8-bit integers (0 to 255)
@@ -36,20 +37,64 @@ type GoNumericTypeInstances struct{
 	//int      		same size as uint
 	Int int
 	Int8 int8
+	Int16 int16
+	Int32 int32
+	Int64 int64
 
 	Uint uint
+	Uint8 uint8
+	Uint16 uint16
+	Uint32 uint32
+	Uint64 uint64
+
+	Float32 float32
+	Float64 float64
+
+	Complex64 complex64
+	Complex128 complex128
+
+	Byte byte
+	Rune rune
 }
 
+//GetAllNumericTypeInstances returns a slice of instance values for each property/field defined on the GoNumericTypeInstances struct.
+//NOTE: NewGetAllNumericTypeInstances instantiates the AllNumericTypeInstances slice.
 func (g *GoNumericTypeInstances) GetAllNumericTypeInstances() []interface{}{
 	return g.AllNumericTypeInstances
 }
 
+//NewGoNumericTypeInstances contructs a new instance of GoNumericTypeInstances.
+//Initializes the AllNumericTypeInstances slice by calling BuildSliceOfAllFieldValuesInObject for performance optimization reasons.
 func NewGoNumericTypeInstances() *GoNumericTypeInstances{
 	goNumericTypeInstances := &GoNumericTypeInstances{}
 	goNumericTypeInstances.AllNumericTypeInstances = BuildSliceOfAllFieldValuesInObject(goNumericTypeInstances)
 	return goNumericTypeInstances
 }
 
+//GoTypeInstances is used to retrieve instances of every primitive type in Go.
+//Useful during testing, but may have other applications.
+type GoTypeInstances struct{
+	AllTypeInstances []interface{}
+	*GoNumericTypeInstances
+}
+
+//GetAllTypeInstances returns a slice containing an instance of every primitive type in Go.
+//Useful when validating that objectToValidate interface{} is of a given type.
+func (g *GoTypeInstances) GetAllTypeInstances() []interface{}{
+	return g.AllTypeInstances
+}
+
+//NewGoTypeInstances creates a new instance of GoTypeInstances
+//initializes slice of AllTypeInstances by appending Numeric type instances, etc together.
+func NewGoTypeInstances() *GoTypeInstances{
+	gti := &GoTypeInstances{
+		GoNumericTypeInstances: NewGoNumericTypeInstances(),
+	}
+	gti.AllTypeInstances = append(gti.AllTypeInstances, gti.GetAllNumericTypeInstances()...)
+	return gti
+}
+
+//helper to reflectively iterate over each Field in a given object, retrieve its value, and return slice of all field values.
 func BuildSliceOfAllFieldValuesInObject(objToReflect interface{})[]interface{}{
 	result := []interface{}{}
 	reflectedObjectValue := reflect.ValueOf(objToReflect)
@@ -62,21 +107,6 @@ func BuildSliceOfAllFieldValuesInObject(objToReflect interface{})[]interface{}{
 	return result
 }
 
-type GoTypeInstances struct{
-	*GoNumericTypeInstances
-}
-
-func (g *GoTypeInstances) GetAllTypeInstances() []interface{}{
-	return g.GetAllNumericTypeInstances()
-}
-
-func NewGoTypeInstances() *GoTypeInstances{
-	goTypeInstances := &GoTypeInstances{
-		GoNumericTypeInstances: NewGoNumericTypeInstances(),
-	}
-	return goTypeInstances
-}
-
 
 var _ = Describe("Validate Type", func() {
 
@@ -86,15 +116,17 @@ var _ = Describe("Validate Type", func() {
 	//helper function to assert that when a schema type is set, all other types should be invalid
 	//calls IsValid on all type instances defined in GoTypeInstances.GetAllTypeInstances
 	//returns true when IsValid returns false for all types other than the schema's Type
-	otherTypesAreInvalid := func(schema Schema) bool{
+	allOtherTypesAreInvalid := func(schema Schema) bool{
 		reflectedSchemaType := schema.GetTypeReflectedType()
+		matchCount := 0
 		for _, t := range goTypeInstances.GetAllTypeInstances() {
 			//don't check IsValid() when the t is the same as the schema type (should be valid in this case, but invalid in all other cases)
 			reflectedTType := reflect.TypeOf(t)
 			if reflectedTType == reflectedSchemaType{
+				matchCount++
 				continue
 			}
-			if schema.Validate(t).IsValid(){
+			if matchCount > 1 || schema.Validate(t).IsValid(){
 				return false
 			}
 		}
@@ -102,17 +134,44 @@ var _ = Describe("Validate Type", func() {
 	}
 
 	Describe("Numeric Type Validation", func(){
+		//ints
 		It("should provide Int function which helps strictly validate that objectToValidate type is int", func() {
 			s := Avoucher().Int()
 			Expect(s.Validate(gti.Int).IsValid()).To(Equal(true))
-			Expect(otherTypesAreInvalid(s)).To(Equal(true))
+			Expect(allOtherTypesAreInvalid(s)).To(Equal(true))
 		})
 
+		It("should provide Int8 function which helps strictly validate that objectToValidate type is int8", func() {
+			s := Avoucher().Int8()
+			Expect(s.Validate(gti.Int8).IsValid()).To(Equal(true))
+			Expect(allOtherTypesAreInvalid(s)).To(Equal(true))
+		})
+
+		It("should provide Int16 function which helps strictly validate that objectToValidate type is int16", func() {
+			s := Avoucher().Int16()
+			Expect(s.Validate(gti.Int16).IsValid()).To(Equal(true))
+			Expect(allOtherTypesAreInvalid(s)).To(Equal(true))
+		})
+
+		It("should provide Int32 function which helps strictly validate that objectToValidate type is int32", func() {
+			s := Avoucher().Int32()
+			Expect(s.Validate(gti.Int32).IsValid()).To(Equal(true))
+			Expect(allOtherTypesAreInvalid(s)).To(Equal(true))
+		})
+
+		It("should provide Int64 function which helps strictly validate that objectToValidate type is int64", func() {
+			s := Avoucher().Int64()
+			Expect(s.Validate(gti.Int64).IsValid()).To(Equal(true))
+			Expect(allOtherTypesAreInvalid(s)).To(Equal(true))
+		})
+
+		//uints
 		It("should provide Uint function which helps strictly validate that objectToValidate type is uint", func() {
 			s := Avoucher().Uint()
 			Expect(s.Validate(gti.Uint).IsValid()).To(Equal(true))
-			Expect(otherTypesAreInvalid(s)).To(Equal(true))
+			Expect(allOtherTypesAreInvalid(s)).To(Equal(true))
 		})
+
 	})
 
 	Describe("Misc Type Validation", func(){
