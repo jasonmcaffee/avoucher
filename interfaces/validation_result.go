6a9094e345069081ffdd86fea93f0bd4bdc6a436
Jason Mcaffee <jason.mcaffee@insidesales.com>
2018-01-13 20:13:04 -0700

    more data added to ValidationResult.

--- interfaces/validation_result.go
+++ interfaces/validation_result.go
@@ -1,8 +1,27 @@
 package interfaces
 
+//ValidationResult is returned by validate functions to indicate whether the test passed, the reason why it failed, etc.
 type ValidationResult interface {
 	IsValid() bool
-	SetIsValid(isValid bool)
+	SetIsValid(bool)
+
+	//GetMessage returns the message set by a validation plugin. Empty string when IsValid() == true.
 	GetMessage() string
-	SetMessage(message string)
+	//SetMessage sets the message indicating why a given test failed.
+	SetMessage(string)
+
+	//SetExpectedValue is used to indicate what a given validation was expecting. (some value on the schema)
+	SetExpectedValue(interface{})
+	//GetExpectedValue returns what the test was expecting. (some value on the schema)
+	GetExpectedValue() interface{}
+
+	//SetActualValue is used to indicate the value a given test received. (objToValidate)
+	SetActualValue(interface{})
+	//GetActualValue returns what the test received. (objToValidate)
+	GetActualValue() interface{}
+
+	//SetTestName is used to indicate which test created the ValidationResult.
+	SetTestName(string)
+	//GetTestName returns the name of the test that created the ValidationResult.
+	GetTestName() string
 }
--- models/validation_result.go
+++ models/validation_result.go
@@ -2,13 +2,16 @@ package models
 
 import . "avoucher/interfaces"
 
+//validationResult is an implementation of the ValidationResult interface
 type validationResult struct {
-	Valid       bool
+	Valid       	bool
 	Message       *string
 	ExpectedValue interface{}
 	ActualValue   interface{}
+	TestName 			string
 }
 
+//NewValidationResult instantiates an object which implements the ValidationResult interface
 func NewValidationResult() ValidationResult {
 	return &validationResult{}
 }
@@ -27,10 +30,42 @@ func (v *validationResult) SetMessage(message string) {
 	v.Message = &message
 }
 
+//IsValid returns a boolean indicating whether validation succeeded or not.
 func (v *validationResult) IsValid() bool{
 	return v.Valid
 }
 
+//SetIsValid sets whether validation succeeded or not.
 func (v *validationResult) SetIsValid(isValid bool){
 	v.Valid = isValid
+}
+
+//SetTestType is used to set the name of the test that produced the ValidationResult
+func (v *validationResult) SetTestName(testType string){
+	v.TestName = testType
+}
+
+//GetTestType returns the name of the test that produced the ValidationResult
+func (v *validationResult) GetTestName() string{
+	return v.TestName
+}
+
+//SetExpectedValue sets the value that the test expected
+func (v *validationResult) SetExpectedValue(expectedValue interface{}){
+	v.ExpectedValue = expectedValue
+}
+
+//GetExpectedValue returns the value that the test expected
+func (v *validationResult) GetExpectedValue() interface{}{
+	return v.ExpectedValue
+}
+
+//SetActualValue sets the value the test encountered in the validated object
+func (v *validationResult) SetActualValue(actualValue interface{}){
+	v.ActualValue = actualValue
+}
+
+//GetActualValue returns the value the test encountered in the validated object
+func (v *validationResult) GetActualValue() interface{}{
+	return v.ActualValue
 }
